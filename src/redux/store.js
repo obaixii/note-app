a28a1911c35cf7d8546d2db08f74689dc30fe9a1
Obaid Awan <obaidawan567@gmail.com>
2023-01-15 20:16:42 +0500

    Added Redux for State Manegment

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+// import { persistReducer, persistStore } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+
 import noteReducer from "../features/note/noteSlice"
+
+// const persistConfig = {
+//     key: 'root',
+//     storage,
+// }
 export const store = configureStore({
     reducer:{
         note:noteReducer
